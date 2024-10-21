f792b1a73d4c958d3e8f93a3b46754450ebaf8a0
Alexander Bikadorov <alex.bikadorov@kdemail.net>
2024-10-21 14:52:38 +0200

    Sort file items by type and name

--- src/main/kotlin/model/FileModel.kt
+++ src/main/kotlin/model/FileModel.kt
@@ -6,13 +6,19 @@ import java.nio.file.attribute.FileTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import kotlin.io.path.*
+
 /**
  * @author Alexander Bikadorov {@literal <goto@openmailbox.org>}
  */
 class FileModel(private val fileDirectory: Path) {
+    enum class ItemType {
+        DIRECTORY,
+        FILE;
+    }
 
-    data class FileItem(val name: String, val size: Long, val lastModifiedTime: FileTime) {
+    data class FileItem(val type: ItemType, val name: String, val size: Long, val lastModifiedTime: FileTime) : Comparable<FileItem> {
         fun humanReadableSize(): String = when {
+            type == ItemType.DIRECTORY -> "<DIR>"
             size >= 1 shl 30 -> "%.1f GB".format(size.toDouble() / (1 shl 30))
             size >= 1 shl 20 -> "%.1f MB".format(size.toDouble() / (1 shl 20))
             size >= 1 shl 10 -> "%.0f kB".format(size.toDouble() / (1 shl 10))
@@ -26,11 +32,25 @@ class FileModel(private val fileDirectory: Path) {
                 .toLocalDateTime()
                 .format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"))
 
+        override fun compareTo(other: FileItem): Int {
+            return Comparator.comparing(FileItem::type)
+                .thenComparing(FileItem::name)
+                .compare(this, other)
+        }
+
     }
 
-    fun items(): List<FileItem> =
-        fileDirectory
+    fun items(): List<FileItem> {
+        fun itemType(path: Path): ItemType =
+            if (path.isRegularFile())
+                ItemType.FILE
+            else
+                ItemType.DIRECTORY
+
+        return fileDirectory
             .toAbsolutePath()
             .listDirectoryEntries()
-            .map { path -> FileItem(path.name, path.fileSize(), path.getLastModifiedTime(LinkOption.NOFOLLOW_LINKS)) }
+            .map { path -> FileItem(itemType(path), path.name, path.fileSize(), path.getLastModifiedTime(LinkOption.NOFOLLOW_LINKS)) }
+            .sortedBy { item -> item }
+    }
 }
