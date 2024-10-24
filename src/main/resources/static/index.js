4d477c8d1fe7f3e70d44a715d434012f5bb11123
Alexander Bikadorov <alex.bikadorov@kdemail.net>
2024-10-24 16:11:00 +0200

    Handle non-file dropping

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,22 +1,40 @@
 function formatBytes(a,b=2){if(!+a)return"0 Bytes";const c=0>b?0:b,d=Math.floor(Math.log(a)/Math.log(1024));return`${parseFloat((a/Math.pow(1024,d)).toFixed(c))} ${["Bytes","KiB","MiB","GiB","TiB","PiB","EiB","ZiB","YiB"][d]}`}
 
 
-const statusMessage = document.getElementById('statusMessage');
+const statusMessage = document.getElementById('status_message');
 function updateStatusMessage(text) {
   statusMessage.textContent = text;
 }
 
+const dropZone = document.getElementById('drop_zone');
 function dragOverHandler(event) {
   event.preventDefault();
 }
 
-function dropHandler(event) {
-    event.preventDefault();
+function dragEnterHandler(event) {
+  if (event.dataTransfer.items.length > 0 &&
+    event.dataTransfer.items[0].kind === 'file') {
+    dropZone.style.backgroundColor = 'honeydew'
+  } else {
+    dropZone.style.backgroundColor = 'lavenderblush'
+  }
+}
 
-    uploadFiles(event.dataTransfer.files);
+function dragLeaveHandler(event) {
+  dropZone.style.backgroundColor = 'aliceblue'
+}
+
+function dropHandler(event) {
+  event.preventDefault();
+
+  uploadFiles(event.dataTransfer.files);
 }
 
 function uploadFiles(files) {
+  if (files.length === 0) {
+    updateStatusMessage('No files to upload!?');
+    return;
+  }
 
   const formData = new FormData();
   for (const file of files) {
